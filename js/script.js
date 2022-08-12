48660cbb940ebd4493741d078eeb55d16e4f4e7a
CODERZ567 <papytumi1@gmail.com>
2022-08-12 12:09:49 +0100

    signup change

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const userName = document.getElementById("name")
+const userEmail = document.getElementById("email")
+const sendMsgBtn = document.getElementById("sendMsgBtn")
+
+sendMsgBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    if(userName.value === "") {
+        const nameError = document.querySelector(".name-error");
+
+        return nameError.innerText = "Please input all necessary information"
+    }
+    else{
+        console.log(userName.value)
+    }
+
+    if(userEmail.value === "") {
+        const emailError = document.querySelector(".email-error");
+
+        return emailError.innerText = "Please input all necessary information"
+    }
+    else{
+        console.log(userEmail.value)
+    }
+
+})
+
